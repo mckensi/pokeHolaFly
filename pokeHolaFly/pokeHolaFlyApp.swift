67c463912f629efab326891c035fbe08ddabcc31
Daniel <daniel.steven.murcia@gmail.com>
2023-11-16 12:48:52 -0500

    Initial Commit

---
+++ pokeHolaFly/pokeHolaFlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokeHolaFlyApp.swift
+//  pokeHolaFly
+//
+//  Created by Daniel Murcia Almanza on 16/11/23.
+//
+
+import SwiftUI
+
+@main
+struct pokeHolaFlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
